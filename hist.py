ed41c23532e729e1478b784137ddb6f539de5ce7
seyhani <aseyhani@icloud.com>
2023-10-26 22:21:14 +0330

    Initial prototype of queue

--- hist.py
+++ hist.py
@@ -2,19 +2,46 @@ from typing import List, Tuple
 
 Packet = Tuple[int, int]
 
+Time = int
+Idx = int
+
 
 class Queue:
     __history: List[Packet]
-    __deqs: List[int]
-    __tails: List[int]
+    __deqs: List[Time]
+    __cap: int
 
-    def __init__(self, total_time: int, hist: List[Packet]):
+    def __init__(self, total_time: Time, cap: Time, hist: List[Packet]):
         self.__hist = hist
         self.__deqs = [0] * total_time
-        self.__tails = [-1] * total_time
+        self.__cap = cap
 
-    def dequeue(self, t: int, count: int):
+    def arrival(self, t: Time) -> List[Packet]:
+        return list(filter(lambda p: p[0] == t, self.__hist))
+
+    def start(self, t: Time) -> Idx:
+        return next(i for i, p in enumerate(self.__hist) if p[0] == t)
+
+    def end(self, t: Time) -> Idx:
+        return len(self.__hist) - 1 - next(i for i, p in enumerate(reversed(self.__hist)) if p[0] == t)
+
+    def enqs(self, t: Time) -> int:
+        return self.end(t) - self.start(t) + 1
+
+    def remain(self, t: Time) -> int:
+        if t == 0:
+            return 0
+        return self.size(t - 1) - self.__deqs[t - 1]
+
+    def acc(self, t: Time) -> int:
+        if t == 0:
+            return 0
+        return min(self.enqs(t), self.__cap - self.remain(t))
+
+    def size(self, t: Time) -> int:
+        if t == 0:
+            return 0
+        return self.remain(t) + self.acc(t)
+
+    def dequeue(self, t: Time, count: int):
         self.__deqs[t] = count
-
-    def cdeq(self, t: int):
-        return sum(self.__deqs[0:t + 1])
--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from hist import Queue
 
 
 def main():
-    q = Queue(5, [(1, 0), (1, 0), (1, 0), (2, 0), (2, 0), (3, 0), (3, 0), (4, 0), (4, 0)])
-    q.dequeue(0, 1)
+    q = Queue(5, 3, [(1, 1), (2, 2), (3, 3)])
     q.dequeue(1, 1)
-    q.dequeue(2, 1)
-    print(q.cdeq(0))
+    print("Enqs[1]: ", q.enqs(1))
+    print("Rem[1]: ", q.remain(1))
+    print("Acc[1]: ", q.acc(1))
+    print("Size[1]: ", q.size(1))
+    print("Rem[2]: ", q.remain(2))
+    print("Acc[2]: ", q.acc(2))
+    print("Size[2]: ", q.size(2))
 
 
 if __name__ == '__main__':
