5e0bc1ae59649fd8be265c5cb645eaa0329cba33
seyhani <aseyhani@icloud.com>
2023-10-27 21:46:23 +0330

    Add order method

--- hist.py
+++ hist.py
@@ -10,14 +10,19 @@ class Queue:
     __history: List[Packet]
     __deqs: List[Time]
     __cap: int
+    __max_t: Time
 
-    def __init__(self, total_time: Time, cap: Time, hist: List[Packet]):
+    def __init__(self, max_t: Time, cap: Time, hist: List[Packet]):
         self.__hist = hist
-        self.__deqs = [0] * total_time
+        self.__deqs = [0] * max_t
         self.__cap = cap
+        self.__max_t = max_t
 
     def arrival(self, t: Time) -> List[Packet]:
         return list(filter(lambda p: p[0] == t, self.__hist))
+
+    def no_arrival(self, t: Time) -> bool:
+        return len(self.arrival(t)) == 0
 
     def start(self, t: Time) -> Idx:
         return next(i for i, p in enumerate(self.__hist) if p[0] == t)
@@ -26,7 +31,9 @@ class Queue:
         return len(self.__hist) - 1 - next(i for i, p in enumerate(reversed(self.__hist)) if p[0] == t)
 
     def enqs(self, t: Time) -> int:
-        return self.end(t) - self.start(t) + 1
+        if self.no_arrival(t):
+            return 0
+        return max(0, self.end(t) - self.start(t) + 1)
 
     def remain(self, t: Time) -> int:
         if t == 0:
@@ -38,10 +45,35 @@ class Queue:
             return 0
         return min(self.enqs(t), self.__cap - self.remain(t))
 
+    def dropped(self, t: Time) -> int:
+        if t == 0:
+            return 0
+        return self.enqs(t) - self.acc(t)
+
     def size(self, t: Time) -> int:
         if t == 0:
             return 0
         return self.remain(t) + self.acc(t)
 
+    def last(self, t: Time) -> Idx:
+        return self.start(t) + self.acc(t) - 1
+
     def dequeue(self, t: Time, count: int):
         self.__deqs[t] = count
+
+    def cacc(self, t: Time):
+        return sum([self.acc(i) for i in range(t + 1)])
+
+    def cdeq(self, t: Time):
+        return sum(self.__deqs[0:t + 1])
+
+    def order(self, i: Idx) -> int:
+        if i == 0:
+            return 0
+        if i <= self.last(self.__hist[i][0]):
+            return self.order(i - 1) + 1
+        else:
+            return self.order(i - 1)
+
+    def head(self, t):
+        return next(i for i, p in enumerate(self.__hist) if self.order(i) == self.cdeq(t - 1))
--- main.py
+++ main.py
@@ -3,15 +3,9 @@ from hist import Queue
 
 
 def main():
-    q = Queue(5, 3, [(1, 1), (2, 2), (3, 3)])
-    q.dequeue(1, 1)
-    print("Enqs[1]: ", q.enqs(1))
-    print("Rem[1]: ", q.remain(1))
-    print("Acc[1]: ", q.acc(1))
-    print("Size[1]: ", q.size(1))
-    print("Rem[2]: ", q.remain(2))
-    print("Acc[2]: ", q.acc(2))
-    print("Size[2]: ", q.size(2))
+    q = Queue(10, 3, [(2, 1), (2, 2), (4, 1), (4, 2), (7, 1), (7, 2)])
+    q.dequeue(5, 1)
+    print(q.head(6))
 
 
 if __name__ == '__main__':
