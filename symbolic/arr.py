c26b81aa10926417c0698cb61ab0d5ad6208247f
seyhani <aseyhani@icloud.com>
2023-10-31 18:22:16 +0330

    Add bool array

--- symbolic/arr.py
+++ symbolic/arr.py
@@ -1,29 +1,19 @@
 from typing import List, Callable
 
-from z3 import Array, IntSort, ModelRef, ExprRef, IntVal, ArrayRef
+from z3 import Array, IntSort, ModelRef, ExprRef, IntVal, ArrayRef, BoolSort
 
 from symbolic.base import SymbolicStructure
 from symbolic.util import eq
 
 
-class BoolArray(SymbolicStructure):
-    size: int
-
-    def constrs(self) -> List[ExprRef]:
-        pass
-
-    def eval(self, model: ModelRef):
-        pass
-
-
-class IntArray(SymbolicStructure):
+class SymbolicArray(SymbolicStructure):
     size: int
     __constrs: List[ExprRef]
 
-    def __init__(self, name, size):
+    def __init__(self, name, size, sort):
         super().__init__(name)
         self.size = size
-        self.arr = Array(name, IntSort(), IntSort())
+        self.arr = Array(name, sort, sort)
         self.__constrs = []
 
     def __getitem__(self, i: int) -> ArrayRef:
@@ -35,8 +25,24 @@ class IntArray(SymbolicStructure):
     def constrs(self) -> List[ExprRef]:
         return self.__constrs
 
-    def eval(self, model: ModelRef) -> List[int]:
+    def eval(self, model: ModelRef):
         return [model.eval(self.arr[t]) for t in range(self.size)]
+
+
+class BoolArray(SymbolicArray):
+    def __init__(self, name, size):
+        super().__init__(name, size, BoolSort())
+
+    def eval(self, model: ModelRef) -> List[bool]:
+        return super().eval(model)
+
+
+class IntArray(SymbolicArray):
+    def __init__(self, name, size):
+        super().__init__(name, size, IntSort())
+
+    def eval(self, model: ModelRef) -> List[int]:
+        return super().eval(model)
 
     @staticmethod
     def create(name: str, ints: List[int]):
